7b550edd999192e34c78774af7ca030bafa4a7f8
Kage Edwards <kage.edwards1992@gmail.com>
2026-09-21 11:09:28 -0700

    Implement max_value() in function_d.py

--- function_d.py
+++ function_d.py
@@ -2,7 +2,12 @@ def max_value(numbers):
     """ This function returns the largest number
         in the list.
     """
-    pass
+    current_max = numbers[0]
+    for number in numbers:
+        if number > current_max:
+            current_max = number
+
+    return current_max
 
 
 if __name__ == "__main__":
